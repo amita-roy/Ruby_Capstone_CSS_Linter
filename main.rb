ecf25a9ffa6d8dbc56a442241fdf0d02a55d6704
Amita <sardana.amita2@gmail.com>
2020-09-01 14:02:35 +0530

    add run method in main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,23 @@
 require 'Crass'
 require_relative './rules/no_duplicate_selector.rb'
+
+def run()
+  path = ARGV.first
+  errors = []
+  File.open(path) do |file|
+    contents = file.read
+    tree = Crass.parse(contents)
+
+    tree.each do |node|
+      if node[:node] == :style_rule
+        errs = verify_selector_rules(node[:selector], tree)
+        errors.concat(errs) unless errs.nil?
+      end
+    end
+  end
+  errors.each do |err|
+    puts err
+  end
+end
+
+run
