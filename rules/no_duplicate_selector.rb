547f42802c33fe1ab04046bb6d3e5a66fd108745
Amita <sardana.amita2@gmail.com>
2020-09-01 13:58:49 +0530

    update no_duplicate_selector

--- rules/no_duplicate_selector.rb
+++ rules/no_duplicate_selector.rb
@@ -4,4 +4,20 @@ class NoDuplicateSelectorRule
   def self.format_tokens(tokens)
     tokens.select { |elem| elem[:node] == :ident }.map { |elem| elem[:value] }
   end
+
+  def self.check(selector, tree)
+    has_duplicates = false
+    tokens = selector[:tokens]
+    selectors = format_tokens(tokens)
+    has_duplicates = selectors.length > selectors.uniq.length
+
+    tree.each do |node|
+      if node[:node] == :style_rule
+        other_selector = format_tokens(node[:selector][:tokens])
+        has_duplicates = selectors.sort == other_selector.sort
+      end
+    end
+
+    return "Duplicate selectors in #{selector[:value].gsub("\n", '')}" if has_duplicates
+  end
 end
