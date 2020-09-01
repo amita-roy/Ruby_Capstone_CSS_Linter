2d4de1249bf4f75077eba7c5dfc7bba460c4b944
Amita <sardana.amita2@gmail.com>
2020-09-01 19:29:21 +0530

    add rule for selector typecase

--- rules/selector_typecase.rb
+++ rules/selector_typecase.rb
@@ -1,14 +1,16 @@
 class SelectorTypeCase
   def self.format_tokens(tokens)
-    tokens.select { |elem| elem[:node] == :ident }.map { |elem| elem[:value] }
+    tokens.select { |elem| elem[:node] == :ident || elem[:node] == :hash }.map { |elem| elem[:value] }
   end
 
   def self.check(selector, tree)
     has_uppercase = false
     tokens = selector[:tokens]
+    p tokens
     selectors = format_tokens(tokens)
-
+    p selectors
     selectors.each do |item|
+      p item
       has_uppercase = true if item =~ /[A-Z]/
     end
 
