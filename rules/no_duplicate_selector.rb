0daf997d6423e443ecc89d3c1a90712777efc3de
Amita <sardana.amita2@gmail.com>
2020-09-01 18:37:17 +0530

    update duplicate_selector class

--- rules/no_duplicate_selector.rb
+++ rules/no_duplicate_selector.rb
@@ -7,16 +7,16 @@ class NoDuplicateSelectorRule
 
   def self.check(selector, tree)
     has_duplicates = false
-
     tokens = selector[:tokens]
-
     selectors = format_tokens(tokens)
     has_duplicates = selectors.length > selectors.uniq.length
 
     tree.each do |node|
       if node[:node] == :style_rule
-        other_selector = format_tokens(node[:selector][:tokens])
-        has_duplicates = true if selectors.sort == other_selector.sort
+        other_selector = node[:selector]
+        tokens = format_tokens(other_selector[:tokens])
+        has_duplicates = true if other_selector[:value] != selector[:value] && selectors.sort == tokens.sort
+        # p has_duplicates
       end
     end
     return "Duplicate selectors in #{selector[:value].gsub("\n", '')}" if has_duplicates
